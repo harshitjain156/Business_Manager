24fc2555589b3b007304d5b945cdb73f85f5bc4a
Harshit jain <harshit2020cs198@abesit.edu.in>
2023-04-29 23:25:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/businessmanager/EmployeeSalary.java
@@ -0,0 +1,14 @@
+package com.example.businessmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmployeeSalary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_salary);
+    }
+}
